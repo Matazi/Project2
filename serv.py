b2c71d67fe646592264c6776fa7fc74379928a7b
Mohammed Hussien <mah445@cornell.edu>
2021-04-29 14:55:44 -0400

    filters

--- serv.py
+++ serv.py
@@ -22,33 +22,44 @@ def worldmap():
 with open('volcanos.json') as f:
     volcanos = json.load( f )
     volcanos = {int(k):v for k,v in volcanos.items()}
-
+eruptions = ""
+with open('eruptions.json') as f:
+    eruptions = json.load( f )
+    eruptions = {int(k):v for k,v in eruptions.items()}
 
 @app.route('/volcanos')
 def volcano():
 
     population_within_5_km = request.args.get('pop5km', None, type=float)
     print(population_within_5_km)
-    population_within_10_km = request.args.get('pop5km', None, type=float)
-    population_within_30_km = request.args.get('pop5km', None, type=float)
-    population_within_100_km = request.args.get('pop5km', None, type=float)
-    primary_volcano_type = request.args.get('pop5km', None, type=float)
+    population_within_10_km = request.args.get('pop10km', None, type=float)
+    population_within_30_km = request.args.get('pop30km', None, type=float)
+    population_within_100_km = request.args.get('pop100km', None, type=float)
+    primary_volcano_type = request.args.get('pokm', None, type=float)
+
+    eruption_category = request.args.get('erCat', None)
 
     vfinal = []
     for v in volcanos.values():
             #Filter criteria
-            if primary_volcano_type is not None:
+            # if primary_volcano_type is not None:
+            #     continue
+            # if population_within_5_km is not None:
+            #     continue
+            # if population_within_10_km is not None:
+            #     continue
+            if population_within_30_km is not None and float(v['population_within_30_km']) < population_within_30_km:
                 continue
-            if population_within_5_km is not None:
-                continue
-            if population_within_10_km is not None:
-                continue
-            if population_within_30_km is not None:
-                continue
-            if population_within_100_km is not None:
-                continue
+            # if population_within_100_km is not None:
+            #     continue
 
             vfinal.append(v)
+    # erfinal = []
+    for v in eruptions.values():
+        if eruption_category is not None and v['eruption_category'] == eruption_category:
+            continue
+        vfinal.append(v)
+    # return jsonify(erfinal)
     return jsonify(vfinal)
 
 
@@ -67,14 +78,19 @@ def event():
         efinal.append(v)
     return jsonify(efinal)
 
+eruptions = ""
 with open('eruptions.json') as f:
     eruptions = json.load( f )
     eruptions = {int(k):v for k,v in eruptions.items()}
 @app.route('/eruptions')
 def eruption():
 
+    eruption_category = request.args.get('erCat', None)
+
     erfinal = []
     for v in eruptions.values():
+        if eruption_category is not None and v['eruption_category'] == eruption_category:
+            continue
         erfinal.append(v)
     return jsonify(erfinal)
 
@@ -85,4 +101,4 @@ def eruption():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='9999')
+    app.run(host='0.0.0.0',port='8000')
